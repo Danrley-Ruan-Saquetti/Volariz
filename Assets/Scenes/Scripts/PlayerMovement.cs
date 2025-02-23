fe155e593283710a4c916a5569907e67be3d07fb
Dan Ruan <danrsaquetti@gmail.com>
2025-02-23 18:03:09 -0300

    feat: Creating mechanics movement and jump of the player

--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -2,7 +2,101 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-  void Update() {
+  [Header("Keybinds")]
+  public KeyCode jumpKey = KeyCode.Space;
 
+
+  [Header("Movement")]
+  public float moveSpeed = 0;
+
+  public float groundDrag;
+
+  public float jumpForce;
+  public float jumpSpeedMultiplier;
+  public float jumpCooldown;
+  bool isReadyToJump;
+
+
+  [Header("Ground Check")]
+  public float playerHeight;
+  public LayerMask groundLayer;
+  bool isGrounded;
+
+  float horizontalInput;
+  float verticalInput;
+
+  public Transform orientation;
+  Vector3 moveDirection;
+
+  Rigidbody rb;
+
+  void Start() {
+    rb = GetComponent<Rigidbody>();
+    rb.freezeRotation = true;
+
+    isReadyToJump = true;
+  }
+
+  void Update() {
+    isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+
+    ReadInput();
+
+    if (Input.GetKey(jumpKey) && isGrounded && isReadyToJump) {
+      Jump();
+    }
+
+    SpeedControl();
+
+    if (isGrounded) {
+      rb.linearDamping = groundDrag;
+    } else {
+      rb.linearDamping = 0;
+    }
+  }
+
+  void FixedUpdate() {
+    MovePlayer();
+  }
+
+  private void ReadInput() {
+    horizontalInput = Input.GetAxisRaw("Horizontal");
+    verticalInput = Input.GetAxisRaw("Vertical");
+  }
+
+  private void MovePlayer() {
+    moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+    Vector3 force = 10f * moveSpeed * moveDirection.normalized;
+
+    if (!isGrounded) {
+      force *= jumpSpeedMultiplier;
+    }
+
+    rb.AddForce(force, ForceMode.Force);
+  }
+
+  private void SpeedControl() {
+    var flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+    if (flatVel.magnitude > moveSpeed) {
+      Vector3 limitedVel = flatVel.normalized * moveSpeed;
+
+      rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+    }
+  }
+
+  private void Jump() {
+    rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+    rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+
+    isReadyToJump = false;
+
+    Invoke(nameof(ResetJump), jumpCooldown);
+  }
+
+  private void ResetJump() {
+    isReadyToJump = true;
   }
 }
